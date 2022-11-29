2a96293edb2e158fcb75e760950d85110b9ab77a
olufemiibikunle <ibikunleolufemi@gmail.com>
2022-11-29 06:31:46 -0800

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
+# Author - Olufemi Ibikunle
+"""Print the numbers from 1 to 100 separated by a space.
+  For multiples of three, print Fizz instead of the number
+  For multiples of five, print Buzz instead of the number.
+  For multiples of three and five, print FizzBuzz instead of the number.
+  """
 
 
 def fizzbuzz():
-    for x in range(1, 101):
-        if x % 3 == 0 and x % 5 == 0:
-            print("FizzBuzz", end=' ')
-        elif x % 3 == 0:
-            print("Fizz", end=" ")
-        elif x % 5 == 0:
-            print("Buzz", end=" ")
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
         else:
-            print("{:d}".format(x), end=" 
+            print("{} ".format(number), end="")
