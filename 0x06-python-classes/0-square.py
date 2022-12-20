ba9b62b36ca775123a9aaecc856f65d7aebcb574
olufemiibikunle <ibikunleolufemi@gmail.com>
2022-12-20 03:30:10 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-Square = __import__('0-square').Square
+""" Module Sqaure """
 
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+
+class Square:
+    """Empty Square class"""
+    pass
