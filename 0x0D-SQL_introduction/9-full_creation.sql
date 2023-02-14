514eaa75e465d097c751270e8030388700c617c7
olufemiibikunle <ibikunleolufemi@gmail.com>
2023-02-14 13:24:29 -0800

    task

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,3 @@
-
- to file
-
--- CREATE SECOND TABLE and insert into
-CREATE TABLE IF NOT EXISTS second_table (id int, name varchar(256),score int);
-INSERT INTO second_table (id, name, score) value (1, 'John', 10);
-INSERT INTO second_table (id, name, score) value (2, 'Alex', 3);
-INSERT INTO second_table (id, name, score) value (3, 'Bob', 14);
-INSERT INTO second_table (id, name, score) value (4, 'George', 8);
+-- creates a table second_table
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table(id, name, score) VALUES(1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
